9c609265e5e2bb1219a031bfdf9835f4ee506da7
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-09-16 14:00:16 +0200

    affichage du composant

---
+++ src/pages/edit/new.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    import Formulaire from '../../components/FormulaireOffreMaison.vue'
+</script>
+
+
+<template>
+    <main>
+        <Formulaire />
+    </main>
+</template>
