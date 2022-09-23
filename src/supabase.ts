9be79c24794bc0c0bad1327eba4d95e64f78b75b
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-09-23 15:08:30 +0200

    modif supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,12 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
+import { ref } from '@vue/reactivity'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
 export const user = ref(supabase.auth.user())
-supabase.auth.onAuthStateChange(()=>{
- user.value = supabase.auth.user()
+supabase.auth.onAuthStateChange(() => {
+    user.value = supabase.auth.user()
 })
 
 // Pour tester dans la console, dé-commenter la ligne qui suit :
