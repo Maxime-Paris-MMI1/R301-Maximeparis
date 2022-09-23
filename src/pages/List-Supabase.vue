32ddfd7f85331776c9c2d34acfc3995509ce300b
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-09-23 14:46:29 +0200

    importation des card grace a supabase

--- src/pages/List-Supabase.vue
+++ src/pages/List-Supabase.vue
@@ -1,14 +1,23 @@
 <template>
     <div class="p-5">
         <h1 class="text-2xl">Page Liste Supabase</h1>
-        <div v-for="maisons in maisons" :key="maisons.nom">
+        <div v-for="maisons in Tableaudesmaisons" :key="maisons.nom">
             <card class="w-1/2 mb-10" v-bind="maisons"/>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+    import card from "../components/Card.vue"
     import { supabase } from "../supabase";
     console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
-    const maisons = []; // à remplacer par l'appel à Supabase
+    //const maisons = []; // à remplacer par l'appel à Supabase
+    
+    let { data: Tableaudesmaisons, error:error } = await supabase
+  .from('Maison')
+  .select('*')
+
+  console.log(Tableaudesmaisons)
+
 </script>
+
