a07013d1c571bcc50eec9f26e1b865ed7992f6d7
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-09-23 13:49:26 +0200

    création de la page list supabase

---
+++ src/pages/List-Supabase.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="p-5">
+        <h1 class="text-2xl">Page Liste fetch</h1>
+        <div v-for="maisons in maisons" :key="maisons.nom">
+            <card class="w-1/2 mb-10" v-bind="maisons"/>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { supabase } from "../supabase";
+    console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
+    const maisons = []; // à remplacer par l'appel à Supabase
+</script>
