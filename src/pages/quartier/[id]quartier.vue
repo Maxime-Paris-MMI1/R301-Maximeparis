bba8e720cb0855f1e77b37cf759276436a8e63ac
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-10-01 15:21:44 +0200

    ajout du contenu [id]quartier.vue

--- src/pages/quartier/[id]quartier.vue
+++ src/pages/quartier/[id]quartier.vue
@@ -1,3 +1,11 @@
-<template>
-    <h1>test 2</h1>
-</template>
+<script setup lang="ts">
+    import FormulaireQuartier from "../../components/FormulaireQuartier.vue";
+    defineProps<{
+      id: string;
+    }>();
+    </script>
+    <template>
+      <div class="p-2">
+        <FormulaireQuartier :id="id" />
+      </div>
+    </template>
