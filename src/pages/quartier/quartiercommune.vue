cc0ecd1920c356c941e90309a85afd555ae63589
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-10-01 14:22:00 +0200

    affichage des quartiers dans les communes

--- src/pages/quartier/quartiercommune.vue
+++ src/pages/quartier/quartiercommune.vue
@@ -6,39 +6,36 @@
     DisclosureButton,
     DisclosurePanel,
     } from '@headlessui/vue'
+    import { ChevronRightIcon } from '@heroicons/vue/20/solid'
     const { data, error } = await supabase.from("allusers").select("*");
     if (error) console.log("n'a pas pu charger la table allusers :", error);
 </script>
     
     <template>
       <section class="flex flex-col">
-        <h3 class="text-2xl">Liste des quartiers</h3>
+        <h3 class="text-2xl mb-5">Liste des quartiers</h3>
 
         
-        <ul>
-          <li v-for="quartierObject in (data as any[])">
-            {{ quartierObject.libelle_Commune }} -
-            {{ quartierObject.libelle_Quartier }}
-          </li>
-        </ul>
+
 
         
-        <Disclosure v-for="(listeQuartier, libelleCommune) in groupBy(data,'libelle_Commune')" :key="libelleCommune">
-          <DisclosureButton> </DisclosureButton>
-        </Disclosure>
-
-
-        <Disclosure>
-          <DisclosurePanel>
-            <ul>
-              <li
-              v-for="quartierObject in listeQuartier"
-              :key="quartierObject.code_Quartier"
-              >
-              </li>
-            </ul>
-          </DisclosurePanel>
-
+        <Disclosure v-for="(listeQuartier, libelleCommune) in groupBy(data,'libelle_Commune')" :key="libelleCommune" v-slot="{ open }">
+          <ul>
+            <li v-for="quartierObject in (data as any[])">
+              <div class="flex justify-start mb-5">
+                <DisclosureButton> 
+                  {{ quartierObject.libelle_Commune }}
+                </DisclosureButton> 
+                <ChevronRightIcon :class="open && 'rotate-90 transform'" class="w-5 h-5" />
+              </div>
+              <DisclosurePanel>
+                <li v-for="quartierObject in listeQuartier" :key="quartierObject.code_Quartier" class="mb-5 font-bold">
+                  {{ quartierObject.libelle_Quartier }}
+                </li>
+              </DisclosurePanel>
+            </li>
+          </ul>
+          
         </Disclosure>
 
 
