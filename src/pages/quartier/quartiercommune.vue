37603048f377f5dbfe72cc0acca57e48e19b5338
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-10-01 13:47:33 +0200

    affichage quartier + test disclosure pannel

--- src/pages/quartier/quartiercommune.vue
+++ src/pages/quartier/quartiercommune.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
     import { supabase } from "../../supabase";
     import groupBy from "lodash/groupBy";
-    const { data, error } = await supabase.from("quartiercommune").select("*");
-    if (error) console.log("n'a pas pu charger la table quartiercommune :", error);
-    </script>
+    import {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    } from '@headlessui/vue'
+    const { data, error } = await supabase.from("maisonquartier").select("*");
+    if (error) console.log("n'a pas pu charger la table maisonquartier :", error);
+</script>
     
     <template>
       <section class="flex flex-col">
         <h3 class="text-2xl">Liste des quartiers</h3>
-        <ul>
+
+        <Disclosure v-for="(listeQuartier, libelleCommune) in groupBy(data,'libelle_Commune')" :key="libelleCommune">
+          <DisclosureButton></DisclosureButton>
+        </Disclosure>
+
+        <DisclosurePanel>
+          <ul>
           <li v-for="quartierObject in (data as any[])">
             {{ quartierObject.libelle_Commune }} -
             {{ quartierObject.libelle_Quartier }}
           </li>
+
+          <li
+          v-for="quartierObject in listeQuartier"
+          :key="quartierObject.code_Quartier"
+          >
+          
+          </li>
         </ul>
+        </DisclosurePanel>
+
       </section>
     </template>
