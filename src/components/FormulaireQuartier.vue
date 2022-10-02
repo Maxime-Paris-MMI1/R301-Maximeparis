c6a7a7e89d3c7fee44444802f5d85ec31fee1370
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-10-02 17:25:58 +0200

    formulaire fonctionnel -> modif le nom du quartier

--- src/components/FormulaireQuartier.vue
+++ src/components/FormulaireQuartier.vue
@@ -2,24 +2,24 @@
   import { supabase } from "@/supabase";
   import { ref } from "@vue/reactivity";
 
-  const quartier = ref({ });
+  const Quartier = ref({ });
 
     
     import { useRouter } from "vue-router";
     const router = useRouter();
     const props = defineProps(["code_commune"]);
     if (props.code_commune) {
-     // On charge les données du quartier
+     // On charge les données du Quartier
      let { data, error } = await supabase
-     .from("quartier")
+     .from("Quartier")
      .select("*")
      .eq("code_commune", props.code_commune);
-     if (error) console.log("n'a pas pu charger le table quartier :", error);
-     else quartier.value = (data as any[])[0];
+     if (error) console.log("n'a pas pu charger le table Quartier :", error);
+     else Quartier.value = (data as any[])[0];
     }
 
-    async function upsertquartier(dataForm, node) {
-    const { data, error } = await supabase.from("quartier").upsert(dataForm);
+    async function upsertQuartier(dataForm, node) {
+    const { data, error } = await supabase.from("Quartier").upsert(dataForm);
     if (error) node.setErrors([error.message])
     else {
     node.setErrors([]);
@@ -35,10 +35,10 @@
 
 
   <FormKit type="form" :submit-attrs="{ classes: { input: 'bg-indigo-600 px-10 py-2 rounded mt-10 mb-16 hover:motion-safe:animate-pulse text-white' } }" :config="{classes: {input: 'p-1 rounded  border-gray-300 shadow-sm border  hover:bg-gray-200', label: 'text-indigo-800'}}"
-    v-model="quartier"  @submit="upsertquartier">
-      <FormKit name="libelle_Quartier" label="Nom du quartier"/>
+    v-model="Quartier"  @submit="upsertQuartier">
+      <FormKit name="libelle_Quartier" label="Nom du Quartier"/>
       <FormKit name="code_commune" label="Code de la commune"/>
-      <FormKit name="code_Quartier" label="Code du quartier"/>
+      <FormKit name="code_Quartier" label="Code du Quartier"/>
   </FormKit>
 
 </template>
