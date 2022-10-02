5dc7071f2c1b9c4875ec11ead774b45fd1b30a33
Maxime-Paris-MMI1 <maxime.paris02@edu.univ-fcomte.fr>
2022-10-02 18:38:13 +0200

    ajout formKit pour les communes

--- src/components/FormulaireQuartier.vue
+++ src/components/FormulaireQuartier.vue
@@ -39,11 +39,18 @@ import { supabase } from "@/supabase";
 
 
 
-  <FormKit type="form" :submit-attrs="{ classes: { input: 'bg-indigo-600 px-10 py-2 rounded mt-10 mb-16 hover:motion-safe:animate-pulse text-white' } }" :config="{classes: {input: 'p-1 rounded  border-gray-300 shadow-sm border  hover:bg-gray-200', label: 'text-indigo-800'}}"
+  <FormKit type="form"  :submit-attrs="{ classes: { input: 'bg-indigo-600 px-10 py-2 rounded mt-10 mb-16 hover:motion-safe:animate-pulse text-white' } }" :config="{classes: {input: 'p-1 rounded  border-gray-300 shadow-sm border  hover:bg-gray-200', label: 'text-indigo-800'}}"
     v-model="Quartier"  @submit="upsertQuartier">
       <FormKit name="libelle_Quartier" label="Nom du Quartier"/>
       <FormKit name="code_commune" label="Code de la commune"/>
       <FormKit name="code_Quartier" label="Code du Quartier"/>
   </FormKit>
 
+
+<!-- Affiche les communes avec comme valeur l'id de la relation -->
+<FormKit
+        type="select"
+        name="code_Commune"
+        label="Commune"
+      />
 </template>
